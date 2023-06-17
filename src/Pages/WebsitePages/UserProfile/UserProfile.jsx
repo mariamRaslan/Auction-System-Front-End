7026edb7d262a0b2c6d45c04ee15d043935e712f
git_projects <mariamraslan231@gmail.com>
2023-06-17 17:58:27 +0200

    user profile and edit profile

--- src/Pages/WebsitePages/UserProfile/UserProfile.jsx
+++ src/Pages/WebsitePages/UserProfile/UserProfile.jsx
@@ -1,78 +1,143 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
-//import Axios
 import axiosInstance from "../../../Axios";
+import jwt_decode from 'jwt-decode';
+import { Link } from "react-router-dom";
+import "./UserProfile.css";
 
-
-//UserProfile
 const UserProfile = () => {
+  const [user, setUser] = useState(null);
+  const decoded = jwt_decode(localStorage.getItem('token'));
+  const id = decoded.id;
 
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await axiosInstance.get(`/users/${id}`);
+        setUser(response.data.data);
+        console.log(response.data.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchUser();
+  }, [id]);
 
-    return (
+  const fakeAuctions = [
+    {
+      id: 1,
+      image: "https://picsum.photos/id/237/200/300",
+      name: "Auction 1",
+      date: "2023-06-20",
+      details: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eget faucibus ex."
+    },
+    {
+      id: 2,
+      image: "https://picsum.photos/id/238/200/300",
+      name: "Auction 2",
+      date: "2023-06-25",
+      details: "Sed quis sapien euismod, fermentum metus vel, efficitur velit."
+    },
+    {
+      id: 3,
+      image: "https://picsum.photos/id/239/200/300",
+      name: "Auction 3",
+      date: "2023-06-30",
+      details: "Fusce vehicula nisl ac lacinia commodo. Nam sit amet semper nisi."
+    }
+  ];
 
-      <div className="container" dir="rtl">
-
-        <section className="mt-5" style={{ backgroundColor: '#eee' }}>
-        <div className="container py-5">
-          <div className="row d-felx align-items-center">
-            <div className="col-lg-4">
-              <div className="card mb-4">
-                <div className="card-body text-center">
-                  <img  className="rounded-circle mb-3" width="160" height="160" alt="avatar" />
-                  <h5 className="my-3">user name </h5>
+  return (
+    <div className="container" dir="rtl">
+      {user && (
+        <section className="mt-3" >
+          <div className="container mt-5">
+            <div className="row d-flex align-items-center p-5" style={{ backgroundColor: "#eee" }}>
+              <div className="col-lg-4">
+                <div className="card mb-4">
+                  <div className="card-body text-center">
+                    <img
+                      className="rounded-circle mb-3"
+                      width="160"
+                      height="160"
+                      src={user.image}
+                      alt="avatar"
+                    />
+                    <h5 className="my-3">{user.name}</h5>
+                    <Link to={`/edit-profile/${id}`} className="btn btn-dark submit-button">Edit</Link>
+                  </div>
+                </div>
+              </div>
+              <div className="col-lg-8">
+                <div className="card mb-4">
+                  <div className="card-body">
+                    <div className="row">
+                      <div className="col-sm-3">
+                        <p className="mb-0">الاسم</p>
+                      </div>
+                      <div className="col-sm-9">
+                        <p className="text-muted mb-0">{user.name}</p>
+                      </div>
+                    </div>
+                    <hr />
+                    <div className="row">
+                      <div className="col-sm-3">
+                        <p className="mb-0">البريد الالكتروني</p>
+                      </div>
+                      <div className="col-sm-9">
+                        <p className="text-muted mb-0">{user.email}</p>
+                      </div>
+                    </div>
+                    <hr />
+                    <div className="row">
+                      <div className="col-sm-3">
+                        <p className="mb-0">رقم الهاتف</p>
+                      </div>
+                      <div className="col-sm-9">
+                        <p className="text-muted mb-0">{user.phone}</p>
+                      </div>
+                    </div>
+                    <hr />
+                    <div className="row">
+                      <div className="col-sm-3">
+                        <p className="mb-0">العنوان</p>
+                      </div>
+                      <div className="col-sm-9">
+                        <p className="text-muted mb-0">
+                          <span>{user.address.city}</span>,
+                          <span>{user.address.street}</span>,
+                          <span>{user.address.building_number}</span>
+                        </p>
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
-            <div className="col-lg-8">
-              <div className="card mb-4">
-                <div className="card-body">
-                  <div className="row">
-                    <div className="col-sm-3">
-                      <p className="mb-0">الاسم</p>
-                    </div>
-                    <div className="col-sm-9">
-                      <p className="text-muted mb-0">user name</p>
-                    </div>
-                  </div>
-                  <hr />
-                  <div className="row">
-                    <div className="col-sm-3">
-                      <p className="mb-0">البريد الالكتروني</p>
-                    </div>
-                    <div className="col-sm-9">
-                      <p className="text-muted mb-0">useremail</p>
-                    </div>
-                  </div>
-                  <hr />
-                  <div className="row">
-                    <div className="col-sm-3">
-                      <p className="mb-0">رقم الهاتف</p>
-                    </div>
-                    <div className="col-sm-9">
-                      <p className="text-muted mb-0">userphone</p>
-                    </div>
-                  </div>
-                  <hr />
-                  <div className="row">
-                    <div className="col-sm-3">
-                      <p className="mb-0">العنوان</p>
-                    </div>
-                    <div className="col-sm-9">
-                      <p className="text-muted mb-0"><span>useraddresscity</span>,<span>useraddressstreet</span>, <span>useraddressbuilding_number</span></p>
-                    </div>
-                  </div>
-                </div>
+            <div className="row my-5 text-center">
+              <div className="col-lg-12 ">
+                <h3>المزادات التي شاركت بها </h3>
               </div>
             </div>
-          </div>
-        </div>
-      </section>   
-      </div>
-
-
-    );
-
-}
-
+            <div className="row p-5 mt-5" style={{ backgroundColor: "#eee" }}>
+              {fakeAuctions.map((auction) => (
+                <div className="col-lg-4 mb-4" key={auction.id}>
+                  <div className="card text-center" style={{ height: "450px" }} >
+<img className="card-img-top" src={auction.image} alt="Auction" style={{ height: "200px" }} />
+<div className="card-body">
+<h4 className="card-title">{auction.name}</h4>
+<p className="card-text">{auction.date}</p>
+<p className="card-text">{auction.details}</p>
+<button className="btn btn-primary submit-button">تفاصيل</button>
+</div>
+</div>
+</div>
+))}
+</div>
+</div>
+</section>
+)}
+</div>
+);
+};
 
 export default UserProfile;
--- src/Pages/WebsitePages/UserProfile/UserProfileEdit.jsx
+++ src/Pages/WebsitePages/UserProfile/UserProfileEdit.jsx
@@ -1,62 +1,101 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-//import Axios
 import axiosInstance from "../../../Axios";
 import "./UserProfile.css";
 
-//UserProfile
 const UserProfileEdit = () => {
-
+  const id = useParams().id;
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [phone, setPhone] = useState("");
   const [city, setCity] = useState("");
   const [street, setStreet] = useState("");
   const [buildingNumber, setBuildingNumber] = useState("");
+  const [image, setImage] = useState(null);
+  const [errors, setErrors] = useState({});
 
-//   useEffect(() => {
-//     // Fetch user data using Axios and the user ID from useParams
-//     axiosInstance.get(`/users/${params.userId}`).then((response) => {
-//       setName(response.data.name);
-//       setEmail(response.data.email);
-//       setPhone(response.data.phone);
-//       setCity(response.data.address.city);
-//       setStreet(response.data.address.street);
-//       setBuildingNumber(response.data.address.buildingNumber);
-//     });
-//   }, [params.userId]);
+  useEffect(() => {
+    axiosInstance.get(`/users/${id}`).then((response) => {
+      console.log(response.data.data)
+      setName(response.data.data.name);
+      setEmail(response.data.data.email);
+      setPhone(response.data.data.phone);
+      setCity(response.data.data.address?.city);
+      setStreet(response.data.data.address?.street);
+      setBuildingNumber(response.data.data.address?.building_number);
+      setImage(response.data.data.image);
+    });
+  }, [id]);
 
-//   const handleSubmit = (event) => {
-//     event.preventDefault();
-//     // Update user data using Axios and the user ID from useParams
-//     axiosInstance.put(`/users/${params.userId}`, {
-//       name,
-//       email,
-//       phone,
-//       address: { city, street, buildingNumber }
-//     }).then((response) => {
-//       // Handle successful update
-//     }).catch((error) => {
-//       // Handle error
-//     });
-//   };
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const errors = {};
+    if (!name) {
+      errors.name = "يرجى ادخال الاسم";
+    }
+    if (!email) {
+      errors.email = "يرجى ادخال البريد الالكتروني";
+    } else if (!/\S+@\S+\.\S+/.test(email)) {
+      errors.email = "يرجى ادخال بريد الكتروني صحيح";
+    }
+    if (!phone) {
+      errors.phone = "يرجى ادخال رقم الهاتف";
+    } else if (!/^[0-9]+$/.test(phone)) {
+      errors.phone = "يرجى ادخال رقم هاتف صحيح";
+    }
+    if (!city) {
+      errors.city = "يرجى ادخال اسم المدينة";
+    }
+    if (!street) {
+      errors.street = "يرجى ادخال اسم الشارع";
+    }
+    if (!buildingNumber) {
+      errors.buildingNumber = "يرجى ادخال رقم المبنى";
+    }
+
+    if (Object.keys(errors).length === 0) {
+      const formData = new FormData();
+      formData.append("name", name);
+      formData.append("email", email);
+      formData.append("phone", phone);
+      formData.append("city", city);
+      formData.append("street", street);
+      formData.append("buildingNumber", buildingNumber);
+      if (image) {
+        formData.append("image", image);
+      }
+      try {
+        await axiosInstance.patch(`/users/${id}`, formData, {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        });
+        // Handle successful update
+      } catch (error) {
+        // Handle error
+      }
+    } else {
+      setErrors(errors);
+    }
+  };
 
   return (
     <div className="container" dir='rtl'>
       <section className="mt-5" style={{ backgroundColor: '#eee' }}>
         <div className="container py-5">
           <div className="row d-felx align-items-center ">
-          <h3 className="text-center">تعديل بيانات المستخدم</h3>
+            <h3 className="text-center">تعديل بيانات المستخدم</h3>
             <div className="col m-4">
               <div className="card">
                 <div className="card-body">
-                  <form >
+                  <form onSubmit={handleSubmit}>
                     <div className="row mb-3">
                       <div className="col-sm-3">
                         <label htmlFor="name" className="form-label">الاسم</label>
                       </div>
                       <div className="col-sm-9">
                         <input type="text" className="form-control" id="name" value={name} onChange={(event) => setName(event.target.value)} />
+                        {errors.name && <p className="text-danger">{errors.name}</p>}
                       </div>
                     </div>
                     <div className="row mb-3">
@@ -65,14 +104,16 @@ const UserProfileEdit = () => {
                       </div>
                       <div className="col-sm-9">
                         <input type="email" className="form-control" id="email" value={email} onChange={(event) => setEmail(event.target.value)} />
+                        {errors.email && <p className="text-danger">{errors.email}</p>}
                       </div>
-                    </div>
+                   </div>
                     <div className="row mb-3">
                       <div className="col-sm-3">
                         <label htmlFor="phone" className="form-label">رقم الهاتف</label>
                       </div>
                       <div className="col-sm-9">
-                        <input type="tel" className="form-control" id="phone" value={phone} onChange={(event) => setPhone(event.target.value)} />
+                        <input type="text" className="form-control" id="phone" value={phone} onChange={(event) => setPhone(event.target.value)} />
+                        {errors.phone && <p className="text-danger">{errors.phone}</p>}
                       </div>
                     </div>
                     <div className="row mb-3">
@@ -81,6 +122,7 @@ const UserProfileEdit = () => {
                       </div>
                       <div className="col-sm-9">
                         <input type="text" className="form-control" id="city" value={city} onChange={(event) => setCity(event.target.value)} />
+                        {errors.city && <p className="text-danger">{errors.city}</p>}
                       </div>
                     </div>
                     <div className="row mb-3">
@@ -89,17 +131,27 @@ const UserProfileEdit = () => {
                       </div>
                       <div className="col-sm-9">
                         <input type="text" className="form-control" id="street" value={street} onChange={(event) => setStreet(event.target.value)} />
+                        {errors.street && <p className="text-danger">{errors.street}</p>}
                       </div>
                     </div>
                     <div className="row mb-3">
                       <div className="col-sm-3">
-                        <label htmlFor="building-number" className="form-label">رقم المبنى</label>
+                        <label htmlFor="buildingNumber" className="form-label">رقم المبنى</label>
                       </div>
                       <div className="col-sm-9">
-                        <input type="text" className="form-control" id="building-number" value={buildingNumber} onChange={(event) => setBuildingNumber(event.target.value)} />
+                        <input type="text" className="form-control" id="buildingNumber" value={buildingNumber} onChange={(event) => setBuildingNumber(event.target.value)} />
+                        {errors.buildingNumber && <p className="text-danger">{errors.buildingNumber}</p>}
                       </div>
                     </div>
-                    <button type="submit" className="btn btn-primary submit-button">حفظ التعديلات</button>
+                    <div className="row mb-3">
+                      <div className="col-sm-3">
+                        <label htmlFor="image" className="form-label">الصورة الشخصية</label>
+                      </div>
+                      <div className="col-sm-9">
+                        <input type="file" className="form-control" id="image" onChange={(event) => setImage(event.target.files[0])} />
+                      </div>
+                    </div>
+                    <button type="submit" className="btn btn-primary">تحديث البيانات</button>
                   </form>
                 </div>
               </div>
